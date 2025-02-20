0adea2d8f002e38c3eb7d03486ca0d48d7d828d5
Va6im <152015622+Va6im@users.noreply.github.com>
2025-02-20 15:20:39 +0800

    Add files via upload

---
+++ php/hunting.php
@@ -0,0 +1,108 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Товары</title>
+    <link rel="stylesheet" href="stylex/stylex-tovar.css">
+</head>
+<body>
+    <header class="header">
+        <a href="#1" class="header__logo">МирОхотник</a>
+        <ul class="header__nav">
+            <li><a href="./index.php">Главная</a></li>
+            <li><a href="#!">Товар</a></li>
+            <li><a href="#!">О себе</a></li>
+            <li><a href="#!">Контакты</a></li>
+        </ul>
+        <a href="./register.php" class="btn">Регистрация</a>
+    </header>
+    
+    <div class="products">
+        <div class="product">
+            <img src="./photo/2tovar.jpg" alt="Product 1">
+            <p>Описание товара 1</p>
+            <a href="!#" class="products_btn">Купить</a>
+        </div>
+        <div class="product">
+            <img src="./photo/2tovar.jpg" alt="Product 2">
+            <p>Описание товара 2</p>
+            <a href="!#" class="products_btn">Купить</a>
+        </div>
+        <div class="product">
+            <img src="./photo/2tovar.jpg" alt="Product 3">
+            <p>Описание товара 3</p>
+            <a href="!#" class="products_btn">Купить</a>
+        </div>
+        <div class="product">
+            <img src="./photo/2tovar.jpg" alt="Product 4">
+            <p>Описание товара 4</p>
+            <a href="!#" class="products_btn">Купить</a>
+        </div>
+        <div class="product">
+            <img src="./photo/2tovar.jpg" alt="Product 5">
+            <p>Описание товара 5</p>
+            <a href="!#" class="products_btn">Купить</a>
+        </div>
+        <div class="product">
+            <img src="./photo/2tovar.jpg" alt="Product 6">
+            <p>Описание товара 6</p>
+            <a href="!#" class="products_btn">Купить</a>
+        </div>
+        <div class="product">
+            <img src="./photo/2tovar.jpg" alt="Product 7">
+            <p>Описание товара 7</p>
+            <a href="!#" class="products_btn">Купить</a>
+        </div>
+        <div class="product">
+            <img src="./photo/2tovar.jpg" alt="Product 8">
+            <p>Описание товара 8</p>
+            <a href="!#" class="products_btn">Купить</a>
+        </div>
+        <div class="product">
+            <img src="./photo/2tovar.jpg" alt="Product 10">
+            <p>Описание товара 9</p>
+            <a href="!#" class="products_btn">Купить</a>
+        </div>
+        <div class="product">
+            <img src="./photo/2tovar.jpg" alt="Product 11">
+            <p>Описание товара 10</p>
+            <a href="!#" class="products_btn">Купить</a>
+        </div>
+        <div class="product">
+            <img src="./photo/2tovar.jpg" alt="Product 12">
+            <p>Описание товара 11</p>
+            <a href="!#" class="products_btn">Купить</a>
+        </div>
+        <div class="product">
+            <img src="./photo/2tovar.jpg" alt="Product 12">
+            <p>Описание товара 12</p>
+            <a href="!#" class="products_btn">Купить</a>
+        </div>
+        <div class="product">
+            <img src="./photo/2tovar.jpg" alt="Product 13">
+            <p>Описание товара 13</p>
+            <a href="!#" class="products_btn">Купить</a>
+        </div>
+        <div class="product">
+            <img src="./photo/2tovar.jpg" alt="Product 14">
+            <p>Описание товара 14</p>
+            <a href="!#" class="products_btn">Купить</a>
+        </div>
+        <div class="product">
+            <img src="./photo/2tovar.jpg" alt="Product 15">
+            <p>Описание товара 15</p>
+            <a href="!#" class="products_btn">Купить</a>
+        </div>
+        <div class="product">
+            <img src="./photo/2tovar.jpg" alt="Product 16">
+            <p>Описание товара 16</p>
+            <a href="!#" class="products_btn">Купить</a>
+        </div>
+    </div>
+
+    <footer>
+        &copy; 2020-2024 год. МирОхотник.ру - интернет-магазин товаров для рыболовов и охотников
+    </footer>
+</body>
+</html>
